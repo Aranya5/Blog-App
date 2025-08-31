a9ddd69362c48f48f0e68d8f8841dab6ddeeaa9c
Aranya5 <aranyabasu5@gmail.com>
2025-08-31 23:00:21 +0530

    Login

--- api/index.jsx
+++ api/index.jsx
@@ -1,12 +1,16 @@
 const express = require("express");
+const app = express();
+app.use(express.json());
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
-const app = express();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
-app.use(cors());
+const secret = 'asdasdasdasdasd';
+
+app.use(cors({credentials:true, origin:'http://localhost:5173'}));
 app.use(express.json());
 
 mongoose.connect(
@@ -26,6 +30,24 @@ app.post("/register", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compare(password, userDoc.password);
+  if(passOk){
+    //logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+      if(err) throw err;
+      res.cookie('token', token).json('ok');
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
+});
+
+
+
 app.listen(4000, () => {
   console.log("Server running on http://localhost:4000");
 });
+
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -9,6 +9,7 @@ function LoginPage() {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
+      credentials: 'include',
     });
   }
   return (
