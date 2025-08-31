e7d15c89cdd9553f575c9bf244addd4f6edefa2c
Aranya5 <aranyabasu5@gmail.com>
2025-08-31 22:00:02 +0530

    Register feature

--- api/index.jsx
+++ api/index.jsx
@@ -1,16 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
 const app = express();
+const bcrypt = require('bcryptjs');
 
+const salt = bcrypt.genSaltSync(10);
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
-  const {username, password} = req.body;
-  res.json({requestData: {username, password}});
-  
+mongoose.connect(
+  "mongodb+srv://basuaranya5_db_user:WXIbCnlZldHTZqcu@cluster0.wi6zwwy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
 
-  res.json('test ok2');
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  // Create a new user in the database
+  try {
+    const userDoc = await User.create({ 
+      username,
+      password: bcrypt.hashSync(password, salt),});
+    res.json(userDoc);
+  } catch (e) {
+    return res.status(400).json(e);
+  }
 });
-//mongosh "mongodb+srv://cluster0.wi6zwwy.mongodb.net/" --apiVersion 1 --username basuaranya5_db_user --password WXIbCnlZldHTZqcu
-app.listen(4000);
+
+app.listen(4000, () => {
+  console.log("Server running on http://localhost:4000");
+});
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,36 @@
-function LoginPage(){
+import { useState } from "react";
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  async function login(ev) {
+    ev.preventDefault();
+    await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
   return (
     <>
-      <form className="login">
+      <form className="login" onSubmit={login}>
         <h1>Login</h1>
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+        <input
+          type="text"
+          placeholder="username"
+          valuue={username}
+          onChange={(ev) => setUsername(ev.target.valueAsDate)}
+        />
+
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
         <button>Login</button>
       </form>
     </>
-  )
+  );
 }
 export default LoginPage;
